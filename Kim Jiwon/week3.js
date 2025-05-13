ee913b8e80056544e5624b5441940da9e9c510ff
bababa117 <162523356+bababa117@users.noreply.github.com>
2025-05-13 19:17:57 +0900

    Create week3.js

---
+++ Kim Jiwon/week3.js
@@ -0,0 +1,27 @@
+let k;
+k = "hello";
+console.log(k);
+
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+console.log(str);
+console.log(str2);
+console.log(phrase);
+
+let age;
+console.log(age);
+
+let user = {
+    name: "John",
+    age: 30
+}
+console.log(user);
+console.log(user.name);
+console.log(user.age);
+
+user["name"]
+user.name = "뱝뱝";
+
+//빈 오브젝트
+let obj = {};
