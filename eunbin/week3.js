908b6fc3f7bfc65d6f7ff184bfa57cc13557f9b2
hong333333 <107095943+hong333333@users.noreply.github.com>
2025-05-13 19:17:57 +0900

    Create week3.js

---
+++ eunbin/week3.js
@@ -0,0 +1,15 @@
+let k = "hello";
+console.log(k);
+
+function a(){
+  let k ="Hello";
+  console.log(K);
+}
+a();
+
+let n=123;
+console.log(n);
+n = 123.456;
+console.log(n);
+console.log(1/0);
+console.log("숫자가 아님"/2);
